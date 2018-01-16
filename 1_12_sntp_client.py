c06e0c1f781002e5348a97ea861f3728bfc397bf
Wang Jiuding <wangjd@Wangjd-Mac.local>
2018-01-16 15:35:58 +0800

    added 1_1_local_machine_info.py

--- 1_12_sntp_client.py
+++ 1_12_sntp_client.py
@@ -6,13 +6,13 @@ import sys
 import time
 
 NTP_SERVER = "0.uk.pool.ntp.org"
-TIME1970 - 2208988800L
+TIME1970 = 2208988800L
 
 def sntp_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    data = '\xlb' + 47 * '\0'
+    data = '\x1b' + 47 * '\0'
     client.sendto(data, (NTP_SERVER, 123))
-    data, address = client,recvfrom( 1024 )
+    data, address = client.recvfrom( 1024 )
     if data:
         print 'Response received from:', address
     t = struct.unpack( '!12I', data )[10]
